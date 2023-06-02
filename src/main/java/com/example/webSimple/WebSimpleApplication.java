a920d8420e8efd8c108a5dd2bdf7937a068a4bf8
Your Name <you@example.com>
2023-06-01 23:21:01 -0300

    .

--- src/main/java/com/example/webSimple/WebSimpleApplication.java
+++ src/main/java/com/example/webSimple/WebSimpleApplication.java
@@ -1,13 +1,27 @@
 package com.example.webSimple;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.webSimple.entities.Department;
+import com.example.webSimple.entities.Product;
+
 @SpringBootApplication
 public class WebSimpleApplication {
-
+	public static List<Product> productList;
 	public static void main(String[] args) {
+		Department d1 = new Department(1,"Tech");
+		Department d2 = new Department(1,"Pet");
+		
+		Product p1 = new Product(1,"macbook",4000,d1);
+		Product p2 = new Product(2,"macbook pro",5000,d1);
+		Product p3 = new Product(3,"pet house",300,d2);
+		productList=Arrays.asList(p1,p2,p3);
+		
 		SpringApplication.run(WebSimpleApplication.class, args);
 	}
-
+	
 }
--- src/main/java/com/example/webSimple/controller/ProductController.java
+++ src/main/java/com/example/webSimple/controller/ProductController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.webSimple.WebSimpleApplication;
 import com.example.webSimple.entities.Department;
 import com.example.webSimple.entities.Product;
 
@@ -17,19 +18,11 @@ import com.example.webSimple.entities.Product;
 @RequestMapping(value = "/products")
 public class ProductController {
 	
+	List<Product> productList = WebSimpleApplication.productList;
+	
 	@GetMapping
-	public ResponseEntity<List> getObjects() {
-		Department d1 = new Department(1,"Tech");
-		Department d2 = new Department(1,"Pet");
-		
-		Product p1 = new Product(1,"macbook",4000,d1);
-		var p2 = new Product(2,"macbook pro",5000,d1);
-		Product p3 = new Product(3,"pet house",300,d2);
-		
-		List<Product> productList = Arrays.asList(p1,p2,p3);
-		
-		ResponseEntity<List> responseEntity = new ResponseEntity<>(HttpStatusCode.valueOf(200));
-		return responseEntity.ok(productList);
+	public ResponseEntity<List> getObjects() {	
+		return ResponseEntity.ok(productList);
 	}
 	
 	@GetMapping(path = "/{id}")
